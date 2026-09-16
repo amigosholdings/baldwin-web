34b0a52f3b5c9f9e817494cbf3a34a4be4274ced
amigosholdings <threeamigosholdings@gmail.com>
2026-09-16 07:42:01 -0400

    Test D1 schema statement boundaries

---
+++ worker/test/agentSchema.test.js
@@ -0,0 +1,14 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+import { agentSchemaStatements } from '../src/agentSchema.js';
+
+test('agent schema is split into complete D1 statements', () => {
+  const statements = agentSchemaStatements();
+  assert.equal(statements.length, 15);
+  for (const statement of statements) {
+    assert.match(statement, /^CREATE (TABLE|INDEX) IF NOT EXISTS /);
+    assert.doesNotMatch(statement, /;\s*$/);
+  }
+  assert.match(statements[0], /^CREATE TABLE IF NOT EXISTS agent_runs \([\s\S]*\)$/);
+  assert.match(statements.at(-1), /^CREATE INDEX IF NOT EXISTS idx_agent_events_action /);
+});
